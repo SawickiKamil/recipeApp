856541680ad0b7b79d974b0b5c393abef34b74e8
Kamil Sawicki <sawicki_kamil@onet.pl>
2020-01-20 22:06:39 +0100

    Creating index html and html controller

---
+++ src/main/java/recipeProject/RecipeApplication.java
@@ -0,0 +1,13 @@
+package recipeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApplication.class, args);
+    }
+
+}
