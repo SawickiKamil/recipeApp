2d1196301cda4252fcb7557a3b3de3f0283102b3
Kamil Sawicki <sawicki_kamil@onet.pl>
2020-01-25 23:11:15 +0100

    Testing RecipeServiceImpl with Mock

---
+++ src/test/java/recipeProject/service/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package recipeProject.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import recipeProject.model.Recipe;
+import recipeProject.repositories.RecipeRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() throws Exception{
+        Recipe recipe = new Recipe();
+        HashSet recipeSet = new HashSet<>();
+        recipeSet.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeSet);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
