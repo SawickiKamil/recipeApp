4709be654a4a7bffcd847d0d9b1452d06cfad010
Kamil Sawicki <sawicki_kamil@onet.pl>
2020-01-26 12:30:22 +0100

    Adding testing to UnitOfMeasureRepository

---
+++ src/test/java/recipeProject/repositories/UnitOfMeasureRepositoryTest.java
@@ -0,0 +1,40 @@
+package recipeProject.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import recipeProject.model.UnitOfMeasure;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryTest {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() throws Exception{
+
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", unitOfMeasure.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() throws Exception{
+
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", unitOfMeasure.get().getDescription());
+    }
+}
