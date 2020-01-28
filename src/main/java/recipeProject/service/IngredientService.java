77cb62cd191d2d3daf09d63d72aac5a7997a2103
Kamil Sawicki <sawicki_kamil@onet.pl>
2020-01-28 20:36:26 +0100

    Implements view of Ingredients, service and controller

---
+++ src/main/java/recipeProject/service/IngredientService.java
@@ -0,0 +1,11 @@
+package recipeProject.service;
+
+import org.springframework.stereotype.Component;
+import recipeProject.command.IngredientsCommand;
+import recipeProject.model.Ingredient;
+
+@Component
+public interface IngredientService {
+
+    IngredientsCommand findByRecipeIdAndId(Long recipeId, Long ingredientId);
+}
